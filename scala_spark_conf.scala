28a233b56f13645ec197e4a5a814aaeb56615e6a
kmbeyond <kiran.learn7@gmail.com>
2020-03-21 18:14:09 -0500

    Create scala_spark_conf.scala

---
+++ scala_spark_conf.scala
@@ -0,0 +1,29 @@
+
+
+val spark = SparkSession.builder
+  .appName("KM App")
+  .master("yarn")
+  .enableHiveSupport()
+  .getOrCreate()
+  
+
+spark.conf.getAll.foreach(println)
+spark.sql("SET -v").show(200, truncate=false)
+spark.conf.getAll.toList
+
+
+
+//to Map
+val configMap:Map[String, String] = spark.conf.getAll
+OR: var configMap2=spark.sparkContext.getConf.getAll
+
+for ((k,v) <- configMap) println(s"$k -> $v")
+
+
+
+//to DF
+spark.conf.getAll.toList.toDF("param", "val").show(false)
+(RDD: sc.parallelize(spark.conf.getAll.toList).
+
+//Get a particular config property
+spark.conf.get("spark.sql.shuffle.partitions")
