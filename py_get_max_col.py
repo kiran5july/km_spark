ba4810644d07e2c3e00be4398318a12f1e7d3e4a
kmbeyond <kiran.learn7@gmail.com>
2020-03-26 05:42:03 -0500

    Update py_get_max_col.py

--- py_get_max_col.py
+++ py_get_max_col.py
@@ -3,7 +3,7 @@ from datetime import datetime, date, timedelta
 
 sDBName="pdw"
 mx_ext_mrch=spark.table("{}.mrch_mast_bin".format(sDBpdw)).select("extract_date") \
- .filter(f.col("extract_date")>=(date.today()-timedelta(36)).strftime('%Y-%m-%d') ) \
+ .filter(F.col("extract_date")>=(date.today()-timedelta(36)).strftime('%Y-%m-%d') ) \
  .agg(F.max("extract_date")) \
  .collect()[0][0]
  
