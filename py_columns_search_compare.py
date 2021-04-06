9006431a769def2ea8511aabab777dae2da6df80
kmbeyond <kiran.learn7@gmail.com>
2021-04-06 12:19:25 -0400

    Update and rename py_columns_compare_hive_parquet.py to py_columns_search_compare.py

---
+++ py_columns_search_compare.py
@@ -0,0 +1,22 @@
+
+#---columns from parquet data
+lstColumns=spark.read.parquet("/km/data/").columns
+df_parquetColumns=spark.createDataFrame(lstColumns, StringType()).withColumnRenamed("value","col_name")
+df_parquetColumns.count()
+
+
+#---columns from table
+df_tblColumns=spark.sql("DESCRIBE kmdb.orders").select("col_name").filter(F.col("col_name").startswith("#")==False)
+df_tblColumns.count()
+
+#---extra in Parquet
+df_parquetColumns.subtract(df_tblColumns).show(200,False)
+
+#---extra in Hive table
+df_tblColumns.subtract(df_parquetColumns).show(200,False)
+
+
+
+#----- look up or matching for a string in columns
+[i for i in lst if lstColumns.find('order')>=0]
+
