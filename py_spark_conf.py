6319fa40a49292002b04d063539b3e6a8a1175be
kmbeyond <kiran.learn7@gmail.com>
2021-09-07 13:01:27 -0400

    Update py_spark_conf.py

--- py_spark_conf.py
+++ py_spark_conf.py
@@ -2,26 +2,35 @@
 
 from pyspark.sql import SparkSession
 spark=SparkSession.builder.master("yarn").appName('KM Pyspark Job').enableHiveSupport().getOrCreate()
+
 sc=spark.sparkContext
-
-
 
 sc.getConf().getAll()
 for item in sorted(sc.getConf().getAll()): print(item)
 
 
-#Read into Dictionary
+#Read into List
 listConf=sc.getConf().getAll()
+
+for i in listConf: print("{} = {}".format(i[0],i[1]) )
+for k,v in listConf: print "{} = {}".format(k, v)
+
+[p for p in listConf if 'compress' in p[0]]
+
+
+#Read List into Dict
 dictConf={i[0]: i[1] for i in listConf}
 
-#print list & dict
-for i in listConf: print("{} = {}".format(i[0],i[1]) )
 for key in dictConf.keys(): print("{} = {}".format(key, dictConf[key]))
   
 
+  
 #Read into Dataframe
-sc.parallelize(sc.getConf().getAll()).toDF(['Param', 'Val']).show(50, False)
+df_params = sc.parallelize(sc.getConf().getAll()).toDF(['Param', 'Val'])
 
-spark.createDataFrame(sc.getConf().getAll(), ["param","val"]).show(50,False)
+df_params = spark.createDataFrame(sc.getConf().getAll(), ["param","val"])
+
+import pyspark.sql.functions as O
+df_params.filter(O.col('param').contains('compress')).show(10,False)
 
 
