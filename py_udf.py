c0f4649330a18e82a7c262e6b6da83e871d960c9
kmbeyond <kiran.learn7@gmail.com>
2022-02-19 14:58:43 -0600

    Update py_udf.py

--- py_udf.py
+++ py_udf.py
@@ -1,33 +1,41 @@
 
-
-from datetime import datetime
+#---UDF with single column input
 import pyspark.sql.functions as F
 from pyspark.sql.types import StringType
 
-
 def DateStringChangeFormat(date_str):
+  from datetime import datetime
   format_from = '%m/%d/%Y'
   format_to = '%Y-%m-%d'
   return datetime.strptime(date_str, format_from).strftime(format_to)
 #test: DateStringChangeFormat('12/31/2021' )
 
 
-#spark.udf.register("DateStringChangeFormat", DateStringChangeFormat, StringType())
-DateStringChangeFormat = F.udf(lambda x: DateStringChangeFormat(x), StringType())
+udf_DateStringChangeFormat = F.udf(DateStringChangeFormat, StringType())
 
 #test on DF
 df_hist = spark.createDataFrame(['12/31/2021'], StringType()).toDF('tr_date')
-df_hist.withColumn('tr_date2', DateStringChangeFormat(F.col('tr_date'))).show(5)
+df_hist.withColumn('tr_date2', udf_DateStringChangeFormat(F.col('tr_date'))).show(5)
++----------+----------+
+|   tr_date|  tr_date2|
++----------+----------+
+|12/31/2021|2021-12-31|
++----------+----------+
 
 
-#---UDF with multiple arguments
+#---UDF with multiple arguments (formats as input arguments)
 def DateStringChangeFormat3(date_str, from_format, to_format):
+  from datetime import datetime
   return datetime.strptime(date_str, from_format).strftime(to_format)
-#test: DateStringChangeFormat('12/31/2021', '%m/%d/%Y', '%Y-%m-%d')
+#test: DateStringChangeFormat3('12/31/2021', '%m/%d/%Y', '%Y-%m-%d')
 
-DateStringChangeFormat3 = F.udf(lambda x: DateStringChangeFormat3(x[0],x[1],x[2]), StringType())
+udf_DateStringChangeFormat3 = F.udf(DateStringChangeFormat3, StringType())
 
 #test on DF
-df_hist.withColumn('tr_date2', DateStringChangeFormat3(F.col('tr_date'), F.lit('%m/%d/%Y'), F.lit('%Y-%m-%d')).show(5)
-
+df_hist.withColumn('tr_date2', udf_DateStringChangeFormat3(F.col('tr_date'), F.lit('%m/%d/%Y'), F.lit('%Y-%m-%d'))).show(5)
++----------+----------+
+|   tr_date|  tr_date2|
++----------+----------+
+|12/31/2021|2021-12-31|
++----------+----------+
 
